46765f36473987ab044cef461127404051ba2eae
mihaituta <tutamihai@gmail.com>
2024-11-17 23:09:02 +0200

    changed api service from render to koyeb

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     proxy: {
       '/api': {
         //target: 'http://localhost:8080', // Points to your Spring Boot backend
-        target: ' https://infinityfood-api-springboot.onrender.com', // Points to your Spring Boot backend
+        target: ' https://concrete-leanora-firehive-5ade5b1f.koyeb.app', // Points to your Spring Boot backend
         // pathRewrite: { '^/api': '' },
         changeOrigin: true, // Adjusts the origin to match the target URL
         secure: false,
