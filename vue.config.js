5e7430fcd9161020b1a7892d6c08913aef587ebb
mihaituta <tutamihai@gmail.com>
2024-11-17 17:10:23 +0200

    updates for netlify

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,14 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     port: 3000,
-    /*proxy: {
+    proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Points to your Spring Boot backend
+        //target: 'http://localhost:8080', // Points to your Spring Boot backend
+        target: ' https://infinityfood-api-springboot.onrender.com', // Points to your Spring Boot backend
         // pathRewrite: { '^/api': '' },
         changeOrigin: true, // Adjusts the origin to match the target URL
         secure: false,
-      },*/
+      },
+    },
   },
 }
